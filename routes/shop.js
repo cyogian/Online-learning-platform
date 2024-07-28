89268f04b80af740440d9ac7d540bac3f6147bd3
Nishi-1805 <165827696+Nishi-1805@users.noreply.github.com>
2024-07-28 20:18:00 +0530

    Create shop.js

---
+++ routes/shop.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const productsController = require('../controllers/products');
+
+// Home and general routes
+router.get('/', productsController.getHome);
+router.get('/courses', productsController.getCourses);
+router.get('/hire', productsController.redirectHome);
+
+router.get('/contact-us', productsController.getContactUs);
+router.post('/contact-us', productsController.postAddContact);
+router.get('/success', productsController.getSuccess);
+
+// Product-related routes
+router.get('/api/products', productsController.getAllProducts); 
+router.get('/products/:id', productsController.getProductDetail);
+router.get('/products', productsController.getProductsPage); 
+
+
+// Cart-related routes
+router.post('/addToCart', productsController.postAddToCart); 
+router.post('/edit-cart-item', productsController.postEditCartItem); 
+router.post('/remove-from-cart', productsController.postRemoveFromCart);
+router.get('/cart-data', productsController.getCart); 
+router.get('/cart', productsController.getCartPage); 
+
+module.exports = router;
+

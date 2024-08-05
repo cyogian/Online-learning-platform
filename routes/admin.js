d2aa160f0df40299288af8dbcf7a29a8a74f415f
Nishi Mishra <165827696+Nishi-1805@users.noreply.github.com>
2024-08-05 13:00:09 +0530

    Delete routes/admin.js

--- routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-const productsController = require('../controllers/products');
-
-router.post('/add-product', productsController.AddNewProduct);
-router.get('/add-product', productsController.getAddProductPage);
-module.exports = router;
-

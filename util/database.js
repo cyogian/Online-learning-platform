f826838600918d5127f07f40a4124be1a86ecd2a
Nishi Mishra <165827696+Nishi-1805@users.noreply.github.com>
2024-08-05 14:12:01 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,5 +1,14 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('node-connect', 'root', 'Rajran@21210703', {dialect: 'mysql', host: 'localhost' });
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'node-connect',
+  process.env.DB_USER || 'root',
+  process.env.DB_PASSWORD || 'Rajran@21210703',
+  {
+    dialect: 'mysql',
+    host: process.env.DB_HOST || 'localhost'
+  }
+);
 
 module.exports = sequelize;

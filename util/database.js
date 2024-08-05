bdfaa7d3cd105ec1ab3a28368dddcb61e216c4e7
Nishi Mishra <165827696+Nishi-1805@users.noreply.github.com>
2024-08-05 15:12:39 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || 'Rajran@21210703',
   {
     dialect: 'mysql',
-    port:3306,
+    port: process.env.DB_PORT || 3306,
     host: process.env.DB_HOST || 'localhost'
   }
 );

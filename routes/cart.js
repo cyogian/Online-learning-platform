79dc7660cb78cfbfcd0b2db04df5430bf8d23d21
Amar Nath Yogi <amar_yogi@epam.com>
2024-08-11 19:20:10 +0530

    refactor

--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Cart = require('../model/Cart.js');
-const Product = require('../model/Product.js');
+const Cart = require('../models/Cart.js');
+const Product = require('../models/Product.js');
 
 router.post('/addToCart', async (req, res) => {
     try {
